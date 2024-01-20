bd5d7f9939483c5019ff8f0d29f381e9c67b5bf2
sarweshkumartiwari <sarweshtiwari14@gmail.com>
2024-01-20 23:46:01 +0530

    added requst feature

--- index.ts
+++ index.ts
@@ -3,7 +3,16 @@ import http from "node:http";
 const hostname = 'localhost';
 const port = 3000;
 const server = http.createServer((req, res) => {
-  
+  if (req.url==="/test" && req.method==="POST") {
+    res.statusCode=200;
+    let body="";
+    req.on("data",(chunk)=>{
+      console.log(chunk.toString())
+      body+=chunk.toString();
+    }).on("end",()=>{
+      res.end("The request text is "+body)
+    })
+  }
 });
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}`);
